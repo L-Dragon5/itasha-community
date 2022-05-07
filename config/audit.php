3f9d8fb02e44f437df4b2c8eb1f183a0fe20b3fb
L-Dragon5 <lildragon555@gmail.com>
2022-05-07 14:33:22 -0400

    Updated to Laravel 9. Other package updates.

--- config/audit.php
+++ config/audit.php
@@ -29,6 +29,7 @@ return [
         'guards'       => [
             'web',
         ],
+        'resolver'     => OwenIt\Auditing\Resolvers\UserResolver::class,
     ],
 
     /*
@@ -39,12 +40,30 @@ return [
     | Define the User, IP Address, User Agent and URL resolver implementations.
     |
     */
-    'resolver' => [
-        'user'       => OwenIt\Auditing\Resolvers\UserResolver::class,
+    'resolvers' => [
         'ip_address' => OwenIt\Auditing\Resolvers\IpAddressResolver::class,
         'user_agent' => OwenIt\Auditing\Resolvers\UserAgentResolver::class,
         'url'        => OwenIt\Auditing\Resolvers\UrlResolver::class,
     ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Empty Values
+    |--------------------------------------------------------------------------
+    |
+    | Should Audit records be stored when the recorded old_values & new_values
+    | are both empty?
+    |
+    | Some events may be empty on purpose. Use allowed_empty_values to exclude
+    | those from the empty values check. For example when auditing
+    | model retrieved events which will never have new and old values
+    |
+    */
+
+    'empty_values'         => true,
+    'allowed_empty_values' => [
+        'retrieved'
+    ], 
 
     /*
     |--------------------------------------------------------------------------
